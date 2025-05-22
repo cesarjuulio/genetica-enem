74569b77da8be617fba0be45cfea31893f169b07
Júlio César <cesarjulio7359@gmail.com>
2025-05-21 23:39:42 -0300

    Configuração inicial da página simulado

--- simulado.php
+++ simulado.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/fav.jpg" type="image/x-icon">
     <title>Simulado Vestibular - Genética</title>
 </head>
 <body>
-    
+    <?php $pagina_atual = 'simulado'; include 'includes/header.php'; ?>
+
+    <h1>Simulado de Genética</h1>
+    <p>Teste seus conhecimentos sobre genética com este simulado.</p>
+
+    <?php include 'includes/footer.php'; ?>
 </body>
 </html>
