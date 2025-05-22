0dcde15621e4fa85cb0ac01b8e1a2794d3919499
Júlio César <cesarjulio7359@gmail.com>
2025-05-22 16:40:18 -0300

    Update perguntas.php

--- includes/perguntas.php
+++ includes/perguntas.php
@@ -6,7 +6,7 @@ $perguntas = [
     "correta" => 1
   ],
   [
-    "pergunta" => "(ENEM 2021) Em um grupo de roedores, a presença de um gene dominante (A) determina indivíduos com pelagem na cor amarela. Entretanto, em homozigose é letal, ou seja, provoca a morte dos indivíduos no útero. Já o alelo recessivo (a) não é letal e determina a presença de pelos pretos. Com base nessas informações, considere o heredograma:
+    "pergunta" => "(ENEM 2021) Em um grupo de roedores, a presença de um gene dominante (A) determina indivíduos com pelagem na cor amarela. Entretanto, em homozigose é letal, ou seja, provoca a morte dos indivíduos no útero. Já o alelo recessivo (a) não é letal e determina a presença de pelos pretos. Com base nessas informações, considere o heredograma: (IMAGEM)
       Qual é a probabilidade de, na próxima ninhada do casal de roedores que está representado na figura pelos números 7 e 8, nascer uma fêmea de pelagem amarela (representada pelo número 11)?",
     "alternativas" => ["1/4 (25%)", "1/3 (33%)", "1/2 (50%)", "2/3 (66%)", "3/4 (75%)"],
     "correta" => 2
@@ -20,14 +20,31 @@ $perguntas = [
     "correta" => 4
   ],
   [
-    "pergunta" => "Quem pintou a Mona Lisa?",
-    "alternativas" => ["Van Gogh", "Michelangelo", "Leonardo da Vinci", "Picasso"],
+    "pergunta" => "(ENEM 2018)Gregor Mendel, no século XIX, investigou os mecanismos da herança genética observando algumas características de plantas de ervilha, como a produção de sementes lisas (dominante) ou rugosas (recessiva), característica determinada por um par de alelos com dominância completa. Ele acreditava que a herança era transmitida por fatores que, mesmo não percebidos nas características visíveis (fenótipo) de plantas híbridas (resultantes de cruzamentos de linhagens puras), estariam presentes e se manifestariam em gerações futuras.
+    A autofecundação que fornece dados para corroborar a ideia da transmissão dos fatores idealizada por Mendel ocorre entre plantas",
+    "alternativas" => ["híbridas, de fenótipo dominante, que produzem apenas sementes lisas.", "híbridas, de fenótipo dominante, que produzem sementes lisas e rugosas.", "de linhagem pura, de fenótipo dominante, que produzem apenas sementes lisas.", "de linhagem pura, de fenótipo recessivo, que produzem sementes lisas e rugosas", "de linhagem pura, de fenótipo recessivo, que produzem apenas sementes rugosas."],
     "correta" => 2
   ],
   [
-    "pergunta" => "Quanto é 9 x 6?",
-    "alternativas" => ["54", "56", "58", "60"],
-    "correta" => 0
+    "pergunta" => "(ENEM 2017) O heredograma mostra a incidência de uma anomalia genética em um grupo familiar.(IMAGEM) O indivíduo representado pelo número 10, preocupado em transmitir o alelo para a anomalia genética a seus filhos, calcula que a probabilidade de ele ser portador desse alelo é de",
+    "alternativas" => ["0%", "25%", "50%", "67%", "75%"],
+    "correta" => 4
+  ]
+  [
+    "pergunta" => "(ENEM 2015) A fenilcetonúria é uma doença hereditária autossômica recessiva, associada à mutação do gene PAH, que limita a metabolização do aminoácido fenilalanina. Por isso, é obrigatório, por lei, que as embalagens de alimentos, como refrigerantes dietéticos, informem a presença de fenilalanina em sua composição. Uma mulher portadora de mutação para o gene PAH tem tês filhos normais, com um homem normal, cujo pai sofria de fenilcetonúria, devido à mesma mutação no gene PAH encontrada em um dos alelos da mulher.
+    Qual a probabilidade de a quarta criança gerada por esses pais apresentar fenilcetonúria?",
+    "alternativas" => ["0%", "12,5%", "25%", "50%", "75%"],
+    "correta" => 3
+  ]
+  [
+    "pergunta" => "(ENEM 2012)Após a redescoberta do trabalho de Gregor Mendel, vários experimentos buscaram testar a universalidade de suas leis. Suponha um desses experimentos, realizado em um mesmo ambiente, em que uma planta de linhagem pura com baixa estatura (0,6 m) foi cruzada com uma planta de linhagem pura de alta estatura (1,0 m). Na prole (F1) todas as plantas apresentaram estatura de 0,8 m. Porém, na F2 (F1 x F1) os pesquisadores encontraram os dados a seguir.(IMAGEM)Os pesquisadores chegaram à conclusão, a partir da observação da prole, que a altura nessa planta é uma característica que",
+    "alternativas" => ["não segue as leis de Mendel.", "não é herdada e, sim, ambiental.", "apresenta herança mitocondrial.", "é definida por mais de um gene.", "é definida por um gene com vários alelos."],
+    "correta" => 4
+  ]
+  [
+    "pergunta" => "(ENEM 2019) Ao final do experimento, os dois grupos de plantas apresentaram",
+    "alternativas" => ["os genótipos e os fenótipos idênticos.", "os genótipos idênticos e os fenótipos diferentes.", "diferenças nos genótipos e fenótipos.", "o mesmo fenótipo e apenas dois genótipos diferentes.", "o mesmo fenótipo e grande variedade de genótipos."],
+    "correta" => 2
   ]
 ];
 ?>
