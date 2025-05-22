8c7a9ecdda890714dbd71e73118c2d02f55728d4
Júlio César <cesarjulio7359@gmail.com>
2025-05-22 16:03:43 -0300

    Create perguntas.php

---
+++ includes/perguntas.php
@@ -0,0 +1,42 @@
+<?php
+$perguntas = [
+  [
+    "pergunta" => "(ENEM 2024)O desenvolvimento da biotecnologia e da clonagem gênica em procariotos fez com que a produção de proteínas se tornasse mais intensa, rápida e econômica. Para a produção de hormônios, enzimas e proteínas de resistência a drogas, uma variação da técnica de reação em cadeia pela polimerase (PCR, na sigla em inglês) utiliza a enzima transcriptase reversa (RT-PCR), que sintetiza moléculas de DNA complementares a partir de fitas de RNA. " <br> "Nesse contexto, essa técnica é importante para detectar genes",
+    "alternativas" => ["Expressos", "Plasmidais", "Dominantes", "Autossômicos"],
+    "correta" => 1
+  ],
+  [
+    "pergunta" => "Quanto é 7 + 5?",
+    "alternativas" => ["10", "12", "14", "15"],
+    "correta" => 1
+  ],
+  [
+    "pergunta" => "Qual linguagem é usada para criar páginas web?",
+    "alternativas" => ["PHP", "Python", "HTML", "C++"],
+    "correta" => 2
+  ],
+  [
+    "pergunta" => "Quem pintou a Mona Lisa?",
+    "alternativas" => ["Van Gogh", "Michelangelo", "Leonardo da Vinci", "Picasso"],
+    "correta" => 2
+  ],
+  [
+    "pergunta" => "Quanto é 9 x 6?",
+    "alternativas" => ["54", "56", "58", "60"],
+    "correta" => 0
+  ]
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../images/fav.jpg" type="image/x-icon">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
